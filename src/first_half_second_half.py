b2ede0d2e85cbda9795fc9cc0fd7945a88996a0b
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:04:30 -0400

    init

---
+++ src/first_half_second_half.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def first_half_second_half(a):
+    pass
+
+def main():
+    a = np.array([[1, 3, 4, 2],
+                [2, 2, 1, 2]])
+    print(first_half_second_half(a))
+
+if __name__ == "__main__":
+    main()
